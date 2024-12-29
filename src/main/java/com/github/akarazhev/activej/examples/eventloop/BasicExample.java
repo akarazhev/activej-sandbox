fe0ec0e80ae5b173c9e140024f1a0f38a7d41ace
Andrey Karazhev <karazhev@gmail.com>
2024-12-29 22:55:00 +0300

    Added the event loop example.

---
+++ src/main/java/com/github/akarazhev/activej/examples/eventloop/BasicExample.java
@@ -0,0 +1,15 @@
+package com.github.akarazhev.activej.examples.eventloop;
+
+import io.activej.eventloop.Eventloop;
+
+// [START EXAMPLE]
+public final class BasicExample {
+    public static void main(String[] args) {
+        Eventloop eventloop = Eventloop.create();
+
+        eventloop.post(() -> System.out.println("Hello World"));
+
+        eventloop.run();
+    }
+}
+// [END EXAMPLE]
